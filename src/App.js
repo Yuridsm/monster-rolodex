6bba7cefaf39c45a2cb9690be21570c9a96cf82c
Yuri Melo <yurifullstack@gmail.com>
2020-07-27 14:53:22 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [
+			{
+				name: 'Frankenstein',
+				id: 'aj38f'
+			},
+			{
+				name: 'Dracula',
+				id: '90ym4'
+			},
+			{
+				name: 'Zombie',
+				id: '4elk2'
+			},
+			]
+		};
+	}
+
+	render() {
+		return (
+	    <div className="App">
+		     {
+		     	this.state.monsters.map(monster =>
+		     		<h1 key={ monster.id }>{ monster.name }</h1>
+		     	)
+		     }
+	    </div>
+  	);
+	}
+
+}
+
+export default App;
