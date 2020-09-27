569d492ca4c18951723b2825dfaf2ae4a56aa61a
Yuri Melo <yurifullstack@gmail.com>
2020-09-27 18:48:23 -0300

    Updates

---
+++ precache-manifest.e10738a1243892c64f0f92fb773965c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150624a1513d38527dc0768ff125664f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "425c258bd45f2e507942",
+    "url": "/monster-rolodex/static/css/main.e25cc14f.chunk.css"
+  },
+  {
+    "revision": "4e5b14f8cc78336e2372",
+    "url": "/monster-rolodex/static/js/2.4cf0026a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.4cf0026a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "425c258bd45f2e507942",
+    "url": "/monster-rolodex/static/js/main.c2d5f55e.chunk.js"
+  },
+  {
+    "revision": "2dd421ae09cc264f58f0",
+    "url": "/monster-rolodex/static/js/runtime-main.83947a27.js"
+  }
+]);
